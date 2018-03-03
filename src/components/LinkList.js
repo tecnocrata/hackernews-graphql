d0094abc0f882447205be164ee18a44d60b3fd70
enrique.ortuno <enrique@ortuno.net>
2018-03-02 21:45:03 -0500

    Retrieving data from prisma and server side

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react'
 import Link from './Link'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
 
 class LinkList extends Component {
-  render() {
-    const linksToRender = [
-      {
-        id: '1',
-        description: 'Prisma turns your database into a GraphQL API 😎 😎',
-        url: 'https://www.prismagraphql.com',
-      },
-      {
-        id: '2',
-        description: 'The best GraphQL client',
-        url: 'https://www.apollographql.com/docs/react/',
-      },
-    ]
-
-    return (
-      <div>{linksToRender.map(link => <Link key={link.id} link={link} />)}</div>
-    )
-  }
+    render() {
+        // 1
+        if (this.props.feedQueryx && this.props.feedQueryx.loading) {
+          return <div>Loading</div>
+        }
+      
+        // 2
+        if (this.props.feedQueryx && this.props.feedQueryx.error) {
+          return <div>Error</div>
+        }
+      
+        // 3
+        const linksToRender = this.props.feedQueryx.feed.links
+      
+        return (
+          <div>{linksToRender.map(link => <Link key={link.id} link={link} />)}</div>
+        )
+      }
 }
 
-export default LinkList
+// 1
+const FEED_QUERY = gql`
+  # 2
+  query FeedQuery {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+      }
+    }
+  }
+`
+
+// 3
+export default graphql(FEED_QUERY, { name: 'feedQueryx' }) (LinkList)
