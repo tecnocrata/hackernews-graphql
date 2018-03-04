7a0386ee240a6b9572ae536ecd5aa8af1fb060aa
enrique.ortuno <enrique@ortuno.net>
2018-03-03 23:47:45 -0500

    Enable login and validate token on server side

--- server/src/utils.js
+++ server/src/utils.js
@@ -8,7 +8,7 @@ function getUserId(context) {
     const { userId } = jwt.verify(token, APP_SECRET)
     return userId
   }
-
+  //Great way to validate users?!
   throw new Error('Not authenticated')
 }
 
